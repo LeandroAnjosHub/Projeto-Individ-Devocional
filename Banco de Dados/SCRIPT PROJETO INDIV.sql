9aa9de42e3128db4b4e0d3554405a733dcbe81f4
LeandroAnjosHub <leandroanjos.sptech.school>
2024-06-27 17:31:00 -0300

    Scrips BD

---
+++ Banco de Dados/SCRIPT PROJETO INDIV.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE projetoIndividual;
+
+USE projetoIndividual;
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	email VARCHAR(45),
+    senha VARCHAR(45)
+);
+
+CREATE TABLE emocoes (
+	idEmocao INT PRIMARY KEY AUTO_INCREMENT,
+	emocao VARCHAR(30),
+	CONSTRAINT chk_emocao CHECK (emocao IN ('felicidade', 'tristeza', 'raiva', 'ansiedade', 'medo'))	
+);
+
+CREATE TABLE anotacao (
+	idAnotacao INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(30),
+    descricao VARCHAR(500),
+	fkUsuario INT,
+	FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+create table quiz (
+	idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+	qtdAcertos VARCHAR(45),
+    qtdErros VARCHAR(45),
+    fkUsuario INT,
+	FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+
+create table usuarioEmocao (
+	idUsuEmocao INT AUTO_INCREMENT,
+	fkUsuario int,
+    fkEmocao int,
+    foreign key (fkUsuario) references usuario (idUsuario),
+    foreign key (fkEmocao) references emocoes (idEmocao),
+    primary key (idUsuEMocao, fkUsuario, fkEmocao)
+);
+
+ select * from usuario;
+ select * from emocoes;
+ select * from anotacao;
+ select * from quiz;
+ select * from usuarioEmocao;
+ 
+ INSERT INTO usuarioEmocao VALUES (default, 6, (SELECT idEmocao FROM emocoes WHERE emocoes.emocao
+        LIKE 'emocao' order by idEmocao desc limit 1));
+        
+        INSERT INTO quiz VALUES (default, '12', '3', 1);
+ 
+SELECT usuario.nome, quiz.qtdAcertos FROM quiz
+  JOIN usuario on idUsuario = fkUsuario;
+  
+SELECT emocoes.emocao, usuario.idUsuario, usuario.nome FROM usuarioEmocao
+   JOIN emocoes ON idEmocao = fkEmocao JOIN usuario
+     ON idUsuario = fkUsuario;
+     
+SELECT emocoes.emocao, usuario.idUsuario, usuario.nome FROM usuarioEmocao
+   JOIN emocoes ON idEmocao = fkEmocao JOIN usuario
+     ON idUsuario = fkUsuario WHERE idUsuario = 8;     
+     
+SELECT anotacao.titulo, anotacao.descricao, usuario.nome FROM anotacao
+  JOIN usuario ON idUsuario = fkUsuario;
+  
+SELECT nome, qtdAcertos FROM quiz JOIN 
+      usuario ON idUsuario = fkUsuario;
+      
+select nome, qtdAcertos FROM quiz
+  JOIN usuario ON idUsuario = fkUsuario WHERE idUsuario = 8; 
+ 
+SELECT nome, qtdAcertos FROM quiz JOIN 
+      usuario ON idUsuario = fkUsuario WHERE idUsuario = 7;
+      
+SELECT nome, emocao FROM usuarioEmocao 
+  JOIN emocoes on idEmocao = fkEmocao 
+    JOIN usuario ON idUsuario = fkUsuario WHERE idUsuario = 4;
+    
+SELECT usuario.nome, emocoes.emocao 
+    FROM usuarioEmocao 
+    JOIN emocoes ON idEmocao = fkEmocao
+    JOIN usuario ON idUsuario = fkUsuario
+    WHERE idUsuario = 4;
+    
+    
+    
+SELECT qtdAcertos FROM quiz WHERE qtdAcertos >= 7;    
+            
